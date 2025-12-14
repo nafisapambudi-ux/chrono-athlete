c1d3e35441c962350b6ff822b37d8cd66c9a1a60
nafisapambudi-ux <nafisapambudi@unesa.ac.id>
2025-12-15 02:55:27 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 
 import Index from "./pages/Index";
@@ -16,21 +16,21 @@ const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/auth" element={<Auth />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/athlete-comparison" element={<AthleteComparison />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-
-        </TooltipProvider>
-      </AuthProvider>
+      <TooltipProvider>
+        <Sonner />
+        <Toaster />
+        <AuthProvider>
+          <HashRouter>
+            <Routes>
+              <Route path="/" element={<Index />} />
+              <Route path="/auth" element={<Auth />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/athlete-comparison" element={<AthleteComparison />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </HashRouter>
+        </AuthProvider>
+      </TooltipProvider>
     </QueryClientProvider>
   );
 }
