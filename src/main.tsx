5112facf68b4d27077ab069dedce5b59843f5dee
nafisapambudi-ux <nafisapambudi@unesa.ac.id>
2025-12-15 03:03:25 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <div className="dark min-h-screen bg-background text-foreground">
+  <BrowserRouter>
     <App />
-  </div>
+  </BrowserRouter>
 );
